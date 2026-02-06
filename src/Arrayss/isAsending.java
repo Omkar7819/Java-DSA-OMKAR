23ba4d448bc1d210496565df4e75b267987837da
Omkar7819 <omkarpatil974334@gmail.com>
2026-02-06 18:13:12 +0530

    Oops and Import files

--- src/Arrayss/isAsending.java
+++ src/Arrayss/isAsending.java
@@ -1,18 +1,23 @@
 package Arrayss;
 
 public class isAsending {
+
+    public static boolean isAsendingg(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         
-        int[]  arr = ArraysIO.input();
-        for(int i=1; i<arr.length; i++){
-            if(arr[i]>=arr[i-1]){
-                System.out.println("Array is in ascending order");
-                break;
-            }else{
-                System.out.println("Array is not in ascending order");
-                break;
-                
-            }
+        int[] arr = ArraysIO.input();
+        boolean ans = isAsendingg(arr);
+        if(ans){
+            System.out.println("Asending");
+        }else{
+            System.out.println("Not Asending");
         }
         
     }
